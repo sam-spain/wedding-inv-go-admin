7af4b4de870d2a5720aa6e030ffd386b1dfa2e36
sam <sam@samspain.com>
2022-09-13 00:36:48 +0100

    Use samspain.com for https

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import Axios from "axios";
 import VueAxios from "vue-axios";
 import VueCookies from "vue-cookies";
 Axios.defaults.withCredentials = true;
-const baseURL = "http://77.86.108.133:8081";
+const baseURL = "https://samspain.com";
 if (typeof baseURL !== "undefined") {
   Axios.defaults.baseURL = baseURL;
 }
