3ce1346eb0e0e56b888c431ab096130840237b44
Sam Spain <sam@samspain.com>
2022-10-02 00:10:44 +0100

    Log post-build

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -7,6 +7,7 @@ const execa = require("execa");
       await execa("git", ["checkout", "--orphan", "gh-pages"]);
       console.log("Building...");
       await execa("npm", ["run", "build"]);
+      console.log("Finished build");
       // Understand if it's dist or build folder
       const folderName = fs.existsSync("dist") ? "dist" : "build";
       await execa("git", ["--work-tree", folderName, "add", "--all"]);
